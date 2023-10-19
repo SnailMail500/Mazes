4d829a325378529942eadf83862f0a823d54b49f
Alex Snell <alex_snell@icloud.com>
2023-10-19 13:32:00 +0100

    Actually got the thing to work (I hope)

--- Mazes/Program.vb
+++ Mazes/Program.vb
@@ -8,6 +8,7 @@ Module Program
     Dim xCoordCheck As Integer = xCoord
     Dim yCoordCheck As Integer = yCoord
     Dim userInput As Char
+    Dim pureInput As Char
     Dim theEnd As Boolean = False
     'I do not like the amount of global variables I need here.
     Sub Main() 'does literally everything else
@@ -18,19 +19,24 @@ Module Program
         'While True
         '    Beep() (I thought this was funny for a total of about 5 seconds)
         'End While
-        While theEnd = True
+        While theEnd = False
             displayMaze()
-            userInput = Console.ReadKey(True).KeyChar As Char 'what in the name of all that is unholy- do i need the 'userInput =' ?
-            Select Case userInput
-                Case "w"
-                    Call wPressed()
-                Case "s"
-                    Call sPressed()
-                Case "a"
-                    Call aPressed()
-                Case "d"
-                    Call dPressed()
-            End Select
+            userInput = Console.ReadKey(True).KeyChar 'what in the name of all that is unholy- do i need the 'userInput =' ?
+            If mazeGrid(5, 10) = "T " Then
+                Console.WriteLine("Well done!")
+                theEnd = True
+            Else
+                Select Case userInput
+                    Case "w"
+                        Call wPressed()
+                    Case "s"
+                        Call sPressed()
+                    Case "a"
+                        Call aPressed()
+                    Case "d"
+                        Call dPressed()
+                End Select
+            End If
         End While
     End Sub
     Sub mazeBuilder() 'creates the maze as 10x10 2d array
